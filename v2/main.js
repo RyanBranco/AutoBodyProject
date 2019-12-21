3197822ea88f3742fb63c52885bff2bc094878b7
Ryan Branco <ryanbr23@gmail.com>
2019-12-20 16:36:09 -0800

    the only html/css that is left is the last subSection and the placehonder things

--- v2/main.js
+++ v2/main.js
@@ -1,1 +1,7 @@
 /*give ".currentScreen to the section that is on your screen"*/
+function questionClick() {
+    let question = document.getElementsByClassName('question')
+    for (i = 0; i <= question.length; i++) {
+        this ;
+    };
+};
