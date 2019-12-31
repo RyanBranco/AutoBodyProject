b103e429f87ec2b94a87d93e11a3332ba91c7d17
Ryan Branco <ryanbr23@gmail.com>
2019-12-30 20:08:41 -0800

    finished the js for the FAQ section

--- v2/main.js
+++ v2/main.js
@@ -3,3 +3,18 @@ const ourWork = document.getElementById("navOurWork");
 const faq = document.getElementById("navFaq");
 const rental = document.getElementById("navRental");
 const mapLocation = document.getElementById("navLocation");
+
+const questions = document.getElementsByClassName("question");
+
+function questionClick(questionNumber) {
+    let show = 0;
+    for (i = 0; i < questions.length; i++) {
+        if (i === questionNumber && show === 0) {
+            questions[i].classList.add("show");
+            show = 1;
+        } else {
+            questions[i].classList.remove("show");
+            show = 0;
+        }
+    };
+};
